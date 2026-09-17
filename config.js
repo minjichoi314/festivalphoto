9dc34658c1470f8554c75e06ed782c6c23074913
minjichoi314 <minjichoi314@gmail.com>
2026-09-17 19:05:17 +0900

    Update EmailJS configuration with new credentials

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // EmailJS 대시보드에서 발급한 공개 식별자를 입력합니다. 비밀 키나 SMTP 암호는 넣지 마세요.
 export const EMAILJS = {
-  serviceId: 'YOUR_SERVICE_ID',
-  templateId: 'YOUR_TEMPLATE_ID',
-  publicKey: 'YOUR_PUBLIC_KEY',
+  serviceId: 'service_dj0a1tx',
+  templateId: 'template_9wh2k77',
+  publicKey: 'ca62f9qV13UhHgUyq',
 };
