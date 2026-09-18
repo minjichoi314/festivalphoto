1702a2e99d78df1b4f042014e3d6b51c31bab893
minjichoi314 <minjichoi314@gmail.com>
2026-09-18 14:45:31 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const MAIL_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxz8OmnhdA6WVr6yFLe7TzW-Xng3rxurURvanEtKppYaDCfoSiaElUQYaNOZQWSxH0b/execL';
+export const MAIL_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxz8OmnhdA6WVr6yFLe7TzW-Xng3rxurURvanEtKppYaDCfoSiaElUQYaNOZQWSxH0b/exec';
